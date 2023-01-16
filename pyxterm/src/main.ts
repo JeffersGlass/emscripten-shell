2eae7934490d73d3216c3d7c33b2c3af80bc165a
Jeff Glass <glass.jeffrey@gmail.com>
2023-01-16 17:57:02 -0600

    Very basic onkey

--- pyxterm/src/main.ts
+++ pyxterm/src/main.ts
@@ -2,19 +2,43 @@ import { Terminal } from 'xterm'
 import { FitAddon } from 'xterm-addon-fit'
 import "../node_modules/xterm/css/xterm.css"
 
+class PSH {
+    terminal: Terminal
+
+    constructor (terminal: Terminal) {
+        this.terminal = terminal
+        //this.terminal.attachCustomKeyEventHandler(this.onKey)
+        this.terminal.onKey(this.onKey)
+    }
+
+    onKey(e: {key: string, domEvent: KeyboardEvent}, f: void) {
+        console.log(e.key); // getting in browser console but Not in the browser itself
+        this.terminal.write(e.key)
+    }
+}
+
 export default class pyXterm {
     afterSetup(runtime) {
+        console.warn("RUNNING AFTER SETUP")
         const xterm_elements = document.getElementsByTagName("py-xterm")
         for (const el of xterm_elements){
             const term = new Terminal({
                 allowProposedApi: true,
                 cursorBlink: true,
             });
+            //psh = new PSH(term);
+            term.onKey((e: {key: string, domEvent: KeyboardEvent}, f: void) => {
+                console.log(e.key); // getting in browser console but Not in the browser itself
+                term.write(e.key)
+            })
+
             const fit = new FitAddon();
             term.loadAddon(fit)
+
             term.open(el as HTMLElement);
             fit.fit();
-            term.write("Proident irure minim eu nulla reprehenderit.")
+            term.write(String(new Date()))
+            term.write("\r\n")
         }
         
     }
