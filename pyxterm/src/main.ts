daa511b149da581dc8da4b058a84e26ff89f808d
Jeff Glass <glass.jeffrey@gmail.com>
2023-01-16 15:37:48 -0600

    Add xterm-fit-addon

--- pyxterm/src/main.ts
+++ pyxterm/src/main.ts
@@ -1,12 +1,19 @@
 import { Terminal } from 'xterm'
+import { FitAddon } from 'xterm-addon-fit'
 import "../node_modules/xterm/css/xterm.css"
 
 const term = new Terminal();
-const target_id = "terminal";
+const fit = new FitAddon();
 
+term.loadAddon(fit)
+
+const target_id = "terminal";
 const terminal_element = document.getElementById(target_id);
+
 if (terminal_element){
     term.open(terminal_element);
+    fit.fit();
+    term.write("Proident irure minim eu nulla reprehenderit.")
 }
 else{
     console.warn(`No element found with id ${target_id}`);
