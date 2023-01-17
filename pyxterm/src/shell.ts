352afcb019da9f4592f24fc83dbd4df345412abb
Jeff Glass <glass.jeffrey@gmail.com>
2023-01-17 08:20:08 -0600

    Handle newlines and backspaces

--- pyxterm/src/shell.ts
+++ pyxterm/src/shell.ts
@@ -10,7 +10,14 @@ export class Emshell {
     }
 
     onKey(e: {key: string, domEvent: KeyboardEvent}, f: void) {
-        console.log(e.key); // getting in browser console but Not in the browser itself
-        this.terminal.write(e.key)
+        //console.log(e.key); // getting in browser console but Not in the browser itself
+        if (e.domEvent.key === 'Backspace') this.write('\x1b[D \x1b[D')
+        else if (e.key === '\r') this.write('\r\n')
+        else this.write(e.key);
+    }
+
+    write(value: any){
+        const output = String(value).replace(/\n/g, '\n\r')
+        this.terminal.write(output)
     }
 }
